d534696950df836e657a1c698f66acef9b06d152
mark-ruuan <markruaan27@gmail.com>
2018-08-31 15:57:58 +0530

    Documentation remaining

---
+++ get_problem.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+from urllib.request import urlopen
+from sys import argv
+from html.parser import HTMLParser
+
+BREAK = "END OF TESTCASE\n"
+
+class problem_parser(HTMLParser):
+	def __init__(self):
+		HTMLParser.__init__(self)
+		self.reading = False
+		self.input = []
+		self.output = []
+		self.buffer = ""
+		self.xr = 0
+		
+	def handle_starttag(self, tag, attrs):
+		if tag == "pre":
+			self.xr ^= 1
+			self.reading = True
+	
+	def handle_endtag(self, tag):
+		if tag == "pre":
+			self.reading = False
+			if self.xr == 1:
+				self.input.append(self.buffer)
+			else:
+				self.output.append(self.buffer)
+			self.buffer = ""
+	
+	def handle_data(self, data):
+		if self.reading:
+			self.buffer += data + '\n'
+problem_no = argv[1]
+problem_letter = argv[2]
+
+url = 'https://codeforces.com/problemset/problem/' + problem_no + '/' + problem_letter
+problem = urlopen(url)
+flag = 0
+if problem.geturl() != url:
+	flag = 1
+	print("URL RELATED ERROR\n")
+else:
+	print("Fetching Problem", problem_no, problem_letter)
+	text = problem.read()
+	parser = problem_parser()
+	parser.feed(text.decode('utf-8'))
+	if len(parser.input) != len(parser.output):
+		flag = 1
+		print("Error in Parsing the problem")
+	else:
+		inp = open('prac.txt', 'w')
+		inp.write(str(len(parser.input)) + '\n')
+		for test_case in parser.input:
+			inp.write(test_case)
+			inp.write(BREAK)
+		inp.close()
+		out = open('prac.out', 'w')
+		out.write(str(len(parser.output)) + '\n')
+		for test_case in parser.output:
+			out.write(test_case)
+			out.write(BREAK)
+		out.close()
+if flag == 0:
+	print("Fetch successful")
+else:
+	print("Fetch unsuccessful")
---
+++ run.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+from sys import argv
+from subprocess import run, PIPE
+from time import time
+
+BREAK = "END OF TESTCASE\n"
+
+check = len(argv) > 1 and int(argv[1])
+inp = open('prac.txt', 'r')
+out = open('prac.out','r')
+num_cases = int(inp.readline())
+if int(out.readline()) != num_cases and check:
+	print("Error: Number of Testcases in input and output file are different")
+	exit()
+for i in range(1, num_cases + 1):
+	case_input = ""
+	line = inp.readline()
+	while line != BREAK:
+		case_input += line
+		line = inp.readline()
+	start_time = time()
+	output = run(["./a.out"],
+		input=case_input.encode('utf-8'),stdout=PIPE).stdout.decode('utf-8')
+	time_taken = time() - start_time
+	if check:
+		case_output = ""
+		line = out.readline()
+		while line != BREAK:
+			case_output += line
+			line = out.readline()
+		print("Case #{}:{}".format(i, case_output == output))
+		print("Time Taken: {}".format(time_taken))
+		if case_output != output:
+			print("Input:")
+			print(case_input, end = '')
+			print("Your output:")
+			print(output, end ='')
+			print("Correct output:")
+			print(case_output, end = '')
+	else:
+		print("Case #{}:".format(i))
+		print(output, end = '')
+		print("Time Taken: {}".format(time_taken))
+	print()
+inp.close()
+out.close()
